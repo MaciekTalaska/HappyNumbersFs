00975e2ae98bc1ae06c8098e7382717815418f7f
MaciekTalaska <maciek.talaska@gmail.com>
2012-08-25 14:47:53 +0200

    is_happy only computes results & returns them

--- Program.fs
+++ Program.fs
@@ -20,27 +20,43 @@ main()
 
 
 
-let rec happy number (allNumbers: list<int>) =
-    printf "%i" number
+let rec is_happy number (allNumbers: list<int>) =
+    let newList = number :: allNumbers
     match number with
-    | 0 -> false
-    | 1 -> true
+    | 0 -> (false, newList)
+    | 1 -> (true, newList)
     | n ->
-        if (List.exists ( fun elem -> elem = number) allNumbers) then false
+        if (List.exists ( fun elem -> elem = number) allNumbers) then (false, newList)
         else
-            let newList = number:: allNumbers
             let numberAsString = (string number)
             let mutable sumOfSquares = 0
             for digit in numberAsString do
                 let i = ((int digit) - (int '0'))
                 sumOfSquares <- sumOfSquares + i * i
-            printf "->"
-            (happy sumOfSquares newList)
+            (is_happy sumOfSquares newList)
 
-let rec print_sequence (allNumbers: list<int>) =
+let print_sequence (allNumbers: list<int>) =
+    Console.Write("Seq: | ")
     for number in allNumbers do
-        printf "%i" number
+        Console.Write("{0}->", number)
+    Console.Write("\b")
+    Console.WriteLine(" | ")
     ()
 
+let reverseList list = 
+    List.fold (fun acc elem -> elem::acc) [] list
+
+let rec print_sequence_recursive( allNumbers: list<int>) =
+    if allNumbers.Length > 1 then (print_sequence_recursive allNumbers.Tail)
+    printf "%i " allNumbers.[0]
+
+let print_sequence_reverse( numbers: list<int> ) =
+    printf "Seq | "
+    (print_sequence_recursive numbers)
+    printf "|"
+
 for i in 1..100 do
-    Console.WriteLine("{0}   || {1} is: {2}", String.Empty, i, (if (happy i []) = true then "Happy" else "Sad"))
+    let mutable happysad = (is_happy i [])
+    Console.WriteLine("{0} is: {1}", i, (if fst(happysad) = true then "Happy" else "Sad"))
+    (print_sequence_reverse (snd happysad))
+    printf "\n" // go to new line
